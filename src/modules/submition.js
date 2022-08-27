f42b22bdc48f7efc5d25886e4468007e34c0f90a
mwafrika <37586807+GloireIrenge@users.noreply.github.com>
2022-08-27 08:09:24 +0200

    Check submitions

--- src/modules/submition.js
+++ src/modules/submition.js
@@ -1,17 +1,18 @@
-import MyScores from './allScores.js';
+import { scoreInsert } from './api.js';
 
 const submition = document.querySelector('.Forms');
 
-const FormData = (container, scores) => {
-  const obj = {
-    name: submition[0].value,
-    score: submition[1].value,
-  };
-  submition[0].value = '';
-  submition[1].value = '';
-  scores.push(obj);
-  localStorage.setItem('scores', JSON.stringify(scores));
-  MyScores(scores, container);
+const FormData = () => {
+  if (submition[0].value && submition[1].value) {
+    // eslint-disable-next-line no-unused-vars
+    const obj = {
+      name: submition[0].value,
+      score: submition[1].value,
+    };
+    submition[0].value = '';
+    submition[1].value = '';
+    scoreInsert();
+  }
 };
 
 export default FormData;
